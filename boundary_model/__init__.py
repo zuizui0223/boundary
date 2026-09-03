b3df856734d157a5fb5bc6008ec57819f3f843e9
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-03 16:52:39 +0900

    Export observation-rank identification API

--- boundary_model/__init__.py
+++ boundary_model/__init__.py
@@ -8,6 +8,13 @@ from .multichannel_identifiability import (
     residual_equivalence_dimension,
     residual_product,
 )
+from .observation_rank import (
+    RankIdentificationResult,
+    coordinate_anchor_rows,
+    exact_matrix_rank,
+    log_linear_identification,
+    scalar_observation_rank_gain,
+)
 from .calibration_transport_family import (
     AnchorLadderStep,
     SymmetricCalibrationBound,
@@ -22,16 +29,21 @@ from .calibration_transport_family import (
 __all__ = [
     "AnchorLadderStep",
     "ChannelAnchorDimension",
+    "RankIdentificationResult",
     "SymmetricCalibrationBound",
     "SymmetricIdentifiedInterval",
     "anchor_ladder",
     "breakdown_factor",
     "channel_ratio_dimension",
+    "coordinate_anchor_rows",
+    "exact_matrix_rank",
     "identify_with_observed_kappa",
     "log_gauge_basis",
+    "log_linear_identification",
     "observed_kappa",
     "reconstruct_final_channel",
     "residual_equivalence_dimension",
     "residual_product",
+    "scalar_observation_rank_gain",
     "symmetric_interval",
 ]
